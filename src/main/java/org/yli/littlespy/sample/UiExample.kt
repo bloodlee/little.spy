da49d473c13918a840721f262f85876437177ae1
Yongqiang Li <jasonleehust@gmail.com>
2016-12-23 00:41:25 -0800

    Start writing stack viewer.

---
+++ src/main/java/org/yli/littlespy/sample/UiExample.kt
@@ -0,0 +1,13 @@
+package org.yli.littlespy.sample
+
+import javafx.application.Application
+import org.yli.littlespy.ui.LittleSpyStackViewer
+
+/**
+ * Created by yli on 12/23/2016.
+ */
+
+
+fun main(args: Array<String>) {
+    var viewer = LittleSpyStackViewer()
+}
---
+++ src/main/java/org/yli/littlespy/ui/LittleSpyStackViewer.kt
@@ -0,0 +1,17 @@
+package org.yli.littlespy.ui
+
+import javafx.application.Application
+import javafx.stage.Stage
+import sun.security.krb5.internal.APOptions
+
+/**
+ * Created by yli on 12/23/2016.
+ */
+class LittleSpyStackViewer : Application() {
+    override fun start(primaryStage: Stage?) {
+
+    }
+}
+
+
+
